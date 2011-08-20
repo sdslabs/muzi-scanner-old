4b097ebd4cc18530a958441e97da278a9b5571f4
Abhay Rana <capt.n3m0@gmail.com>
2011-08-20 20:19:50 +0530

    Still working on music scanning

--- scan.rb
+++ scan.rb
@@ -1,4 +1,8 @@
 #!/usr/bin/env ruby
+#Time Log at the end :
+
+time = Time.now.to_i;
+require 'ftools'
 require 'rubygems'
 require 'active_record'
 require 'logger'
@@ -6,100 +10,48 @@ require 'yaml'
 require "mp3info"
 require 'image_size'
 require 'ImageResize'
+require 'models'
+require 'modules'
 
-root = ARGV[1] || '/media/Entertainment/Music/Rock'
-
-# @see http://www.ruby-forum.com/topic/178706
-if RUBY_VERSION =~ /1.9/
-    Encoding.default_external = Encoding::UTF_8
-    Encoding.default_internal = Encoding::UTF_8
-end
+root = Dir.pwd
+music_root = ARGV[0] || '/media/Entertainment/Music/'
+puts "Please wait, scanning directory"
 
 dbconfig = YAML::load(File.open('database.yml'))
 ActiveRecord::Base.establish_connection(dbconfig)
 ActiveRecord::Base.logger = Logger.new(File.open('database.log', 'w'))
 
-class Track < ActiveRecord::Base
-	belongs_to :album, :foreign_key => 'album'
-	belongs_to :genre, :foreign_key => 'genre'
-	belongs_to :band, :foreign_key => 'band'
-	belongs_to :year, :foreign_key => 'year'
-	
-	def pic_data(apic)
-		max_size_apic = max_size_folder = 0
-		if apic
-			if apic.is_a?(Array)
-				for i in 0...APIC.length
-					image_size=1
-					text_encoding,mime_type,picture_type,description,picture_data = apic[i].unpack("c Z* c Z* a*")
-					ImageSize.new(picture_data).size.each {|i| image_size*=i}
-					if image_size > max_size_apic 
-						pic_data_apic = picture_data
-						max_size_apic = image_size
-					end
-				end
+
+i=0
+Dir.chdir(music_root)
+for file in  Dir["**/*.{m,M}{P,p}3"]
+	mp3info = Mp3Info.open(file)
+	track = Track.find_by_file(mp3info.filename)
+	if track.blank?
+		track = Track.create(
+			:file	=>	mp3info.filename,
+			:title	=> mp3info.tag.title || mp3info.tag2.TT2 || File.basename(file,".mp3"),
+			:album	=> Album.find_or_create_by_name(mp3info.tag.album || mp3info.tag2.TAL || "Unknown Album"),
+			:genre	=> Genre.find_or_create_by_name(mp3info.tag.genre_s || "Unknown Genre"),
+			:year	=> Year.find_or_create_by_name(mp3info.tag1.year || mp3info.tag2.TDAT || mp3info.tag2.TDRC || mp3info.tag2.TORY || 2000),
+			:artist	=> mp3info.tag.artist || mp3info.tag2.TP1 || "Unknown Artist",
+			:track	=> mp3info.tag.tracknum  || ( mp3info.tag2.TRCK ? mp3info.tag2.TRCK.split('/').first	 : 0 ) ||  0,
+			:band	=> Band.find_or_create_by_name(  mp3info.tag2.TPE2  || mp3info.tag2.TP2 || mp3info.tag2.ALBUMARTIST || mp3info.tag.artist || "Unkown Artist"), #Also called the Album Artist/Band
+			:plays	=> 0,
+			:length	=> mp3info.length
+		)
+		track.save
+		puts file
+		unless File.exists?(root+'/pics/'+track.album.id.to_s)
+			pic_data=track.pic_data(mp3info.tag2.APIC)
+			if pic_data == false
+				File.copy(root+'/unknown.jpg',root+'/pics/'+track.album.id.to_s)
 			else
-				image_size=1
-				text_encoding,mime_type,picture_type,description,picture_data = apic.unpack("c Z* c Z* a*")
-				ImageSize.new(picture_data).size.each {|i| image_size*=i}
-				pic_data_apic = picture_data
-				max_size_apic = image_size;
+				f = File.new(root+'/pics/'+track.album.id.to_s,'w')
+				f<<pic_data
 			end
 		end
-		
-		for image in Dir[File.dirname(self.file)+"/*.{{{p,P}{n,N}{g,G}},{{j,J}{p,P}*{g,G}}}"]
-			image_size=1
-			pic_data_folder = IO.read(image)
-			ImageSize.new(pic_data_folder).size.each {|i| image_size*=i}
-			if image_size > max_size_folder
-				max_size_folder = image_size
-			end
-		end
-		#Now for the decision making part
-		if max_size_folder > max_size_apic
-			return pic_data_folder 
-		else
-			return pic_data_apic
-		end
+		i=i+1
 	end
 end
-
-class Album < ActiveRecord::Base
-	has_many :track
-end
-
-class Genre < ActiveRecord::Base
-	has_many :track
-end
-
-class Year < ActiveRecord::Base
-	has_many :track
-end
-
-class Band < ActiveRecord::Base
-	has_many :track
-end
-
-#Dir.chdir(root)
-for file in  Dir[root+"**/*.{m,M}{P,p}3"]
-	puts file
-	mp3info = Mp3Info.open(file)
-	track = Track.new(
-		:file	=> mp3info.filename,
-		:title	=> mp3info.tag.title || url.split('/').last.split('.').first,
-		:album	=> Album.find_or_create_by_name(mp3info.tag.album || "Unknown Album"),
-		:genre	=> Genre.find_or_create_by_name(mp3info.tag.genre_s || "Unknown Genre"),
-		:year	=> Year.find_or_create_by_name(mp3info.tag1.year || mp3info.tag2.TDAT || mp3info.tag2.TDRC || mp3info.tag2.TORY || 2000),
-		:artist	=> mp3info.tag.artist || "Unknown Artist",
-		:track	=> mp3info.tag.tracknum  || ( mp3info.tag2.TRCK ? mp3info.tag2.TRCK.split('/').first : 0 ) ||  0,
-		:band	=> Band.find_or_create_by_name(mp3info.tag2.TPE2  || mp3info.tag.artist || "Unkown Artist"), #Also called the Album Artist/Band
-		:plays	=> 0,
-		:length	=> mp3info.length
-	)
-	track.save
-	f = File.new('tmp','w')
-	pic_data = track.pic_data(mp3info.tag2.APIC)
-	f<<pic_data
-	Image.resize('tmp','./pics/'+track.album.id.to_s+'.jpg',200,200);
-end
-File.delete('tmp')
+puts "#{Time.now.to_i - time} seconds taken in parsing #{i} songs"
