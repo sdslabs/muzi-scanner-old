c76fa11e7b54f1afb5d03be2750c11c42aac2b3f
Divij Bindlish <dvjbndlsh93@gmail.com>
2014-05-29 04:20:56 +0530

    Completes basic functionality

--- app/utils.rb
+++ app/utils.rb
@@ -9,20 +9,133 @@ def getGenre(string)
   end
 end
 
-def downloadAlbumArt (album, pics)
-  return if File.exist?("#{pics}/#{album.id}.jpg")
-  bandName = Band.find(Track.find_by_album(album.id).band).name
+def downloadAlbumArt (album)
+  return if $albumArtFolder.nil?
+  return if File.exist?("#{$albumArtFolder}/#{album.id}.jpg")
+  band = Band.find(Track.find_by_album(album.id).band)
+  filename = "#{$albumArtFolder}/#{album.id}.jpg"
   begin
-    album_info = $lastfm.album.get_info(bandName, album.name)
-  # rescue
-    # return
-  end
-  return unless album_info['image'].length > 0
-  album_info['image'].each do |img|
-    if img['size'] == 'large' and img['content']
-      filename = "#{pics}/#{album.id}.jpg"
-      `wget -nv "#{img["content"]}" -O #{filename}`
+    response = HTTParty.get("http://www.musicbrainz.org/ws/2/release/?limit=1&fmt=json&query=release:#{URI.encode(album.name)}+artist:#{URI.encode(band.name)}")
+    mbid = JSON.parse(response.body).releases[0].id.to_s
+    response = HTTParty.get("http://www.coverartarchive.org/release/#{URI.encode(mbid)}")
+    JSON.parse(response.body).images.each do |image|
+      if image.types[0] == "Front"
+        url = image.image
+        `wget -nv "#{url}" -O #{filename}`
+        return
+      end
     end
+  rescue
+    return
   end
+  downloadArtistPic(band)
 end
 
+def saveAlbum (album_path)
+  album_path = album_path.strip! || album_path
+  album = AudioInfo::Album.new(album_path)
+  if album.empty?
+    puts 'Album empty'
+    return
+  else
+    puts album_path
+  end
+
+  Dir.chdir(album_path)
+  band_name = File.basename(File.expand_path("..", album_path))
+  album_folder = File.basename(album_path)
+
+  album.files.each do |track|
+    addTrackToDatabase(track, album)
+  end
+
+  downloadAlbumArt(album)
+end
+
+def addTrackToDatabase (track, album, language = "English")
+  return unless ["mp3","m4a","mp4"].include? track.extension.downcase
+
+  # Calculate the path which is stored in database
+  filename_in_database = Pathname.new(track.path).relative_path_from(Pathname.new($musicFolder)).to_s
+
+  # If track is already in database, skip to next
+  if Track.find_by_file filename_in_database
+    return
+  end
+
+  # Default album title is the name of the folder (unless one is
+  # given by the album itself)
+  album_title = album.title.empty? ? album_folder : album.title
+
+  # Year
+  year = nil
+  if track.info.tag1
+    year ||= track.info.tag1.year
+  elsif track.info.tag2
+    year ||= track.info.tag2.TDAT || track.info.tag2.TDRC || track.info.tag2.TORY
+  else
+    year ||= track.info.DAY || "2000"
+  end
+  year = "2000" if year.nil?
+
+  # Artist
+  artist = track.artist || track.info.tag.artist || track.info.tag2.TP1 || "Unknown Artist"
+
+  # Genre
+  if track.info.tag && track.info.tag.genre_s
+    genre = getGenre(track.info.tag.genre_s) || "Unknown Genre"
+  else
+    genre = "Unknown genre"
+  end
+
+  # Track Number
+  track_number   = track.info.tag.tracknum if track.info.tag
+  track_number ||= track.info.tag2.TRCK.split('/').first if track.info.tag2 && track.info.tag2.TRCK
+  track_number ||= 0
+
+  # Title
+  title   = track.title
+  title ||= track.info.tag.title if track.info.tag
+  title ||= track.info.tag2.TT2 if track.info.tag2
+
+  # If all tags fail, use the filename
+  title = File.basename(track.path, "." + track.extension) if title.chomp.length == 0
+
+  track = Track.new(
+    :file   => filename_in_database,
+    :title  => title,
+    :album  => Album.find_or_create_by_name_and_language(album_title, language),
+    :genre  => Genre.find_or_create_by_name(genre),
+    :year   => Year.find_or_create_by_name(year),
+    :artist => artist,
+    :track  => track_number,
+    :band   => Band.find_or_create_by_name_and_language(band_name, language),
+    :plays  => 0,
+    :length => track.length
+  )
+  track.save
+end
+
+def downloadArtistPic (artist)
+  return if $artistPicFolder.nil?
+  return if File.exist?("#{$artistPicFolder}/#{artist.id}.png")
+  filename = "#{$artistPicFolder}/#{album.id}.jpg"
+  zune_root = 'http://catalog.zune.net/v3.2/en-US/music/artist'
+  begin
+    response = HTTParty.get("#{zune_root}?q=#{URI.encode(artist.name)}")
+    xmldoc = Document.new(response.body)
+    xmldoc.elements.each('a:feed/a:entry/a:title') do |element|
+      id = element.text.to_s
+      response = HTTParty.get("#{zune_root}/#{id}/images")
+      xml = Document.new(response.body)
+      xml.elements.each('a:feed/a:entry/instances/imageInstance/url') do |elem|
+        url = elem.text
+        `wget -nv "#{url}" -O #{filename}`
+        break
+      end
+      break
+    end
+  rescue
+    return
+  end
+end
