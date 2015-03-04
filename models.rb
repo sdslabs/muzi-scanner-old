d6ef129cc462b3cc75457c1c42f33afd61157ccb
Amanpreet Singh <apsdehal@gmail.com>
2015-03-05 02:03:11 +0530

    Fixes according to howl

--- models.rb
+++ models.rb
@@ -1,25 +1,25 @@
 class Track < ActiveRecord::Base
-	belongs_to :album, :foreign_key => 'album'
-	belongs_to :genre, :foreign_key => 'genre'
-	belongs_to :band, :foreign_key => 'band'
-	belongs_to :year, :foreign_key => 'year'
+	belongs_to :album, :foreign_key => 'album_id'
+	belongs_to :genre, :foreign_key => 'genre_id'
+	belongs_to :band, :foreign_key => 'band_id'
+	belongs_to :year, :foreign_key => 'year_id'
 	has_many :pic
 end
 
 class Album < ActiveRecord::Base
-	has_many :track
+	has_many :track, :foreign_key => 'album_id'
 end
 
 class Genre < ActiveRecord::Base
-	has_many :track
+	has_many :track, :foreign_key => 'genre_id'
 end
 
 class Year < ActiveRecord::Base
-	has_many :track
+	has_many :track, :foreign_key => 'year_id'
 end
 
 class Band < ActiveRecord::Base
-	has_many :track
+	has_many :track, :foreign_key => 'band_id'
 end
 
 class Pic < ActiveRecord::Base
