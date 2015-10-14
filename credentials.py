e70637cf5c492047a6a339f65080d2ef591f6391
Gautham Goli <goligautham@hotmail.com>
2015-10-14 22:19:10 +0530

    Read credentials from a file

---
+++ credentials.py
@@ -0,0 +1,47 @@
+__author__ = 'Gaurav'
+
+
+import json
+import os
+
+
+def get_credentials_file_path():
+    """
+    Location for the standalone credentials file in json format,
+    Here I am assuming it is in the same directory as this file
+    :return: String
+    """
+    return (os.path.join(os.path.dirname(__file__), 'data.json'))
+
+
+
+def getdata(func):
+    """Decorator to Deserialize json data and passes it to functions """
+    def inner():
+        with open(get_credentials_file_path()) as data_file:
+            # deserialize json data to python object
+            try:
+                # specifically get the credentials under default category
+                data = json.load(data_file)['DEFAULT']
+            except Exception as e:
+                # http://stackoverflow.com/questions/4308182/getting-the-exception-value-in-python
+                print type(e)
+                print e.message
+        # pass the appropriate parameters into the function
+        credential = func(data)
+        return credential
+
+    return inner
+
+
+@getdata
+def get_lastfm_api_key(data):
+    return data["lastfm_api_key"]
+
+
+@getdata
+def get_lastfm_api_secret(data):
+    return data["lastfm_api_secret"]
+
+
+
--- getnames.py
+++ getnames.py
@@ -2,18 +2,36 @@ __author__ = 'admin'
 
 import os
 import sys
+import sqlite3
+import glob
+import eyed3
+import credentials
 
-walk_dir = sys.argv[1]
+artists_directory = sys.argv[1]
 
-print('walk_dir = ' + walk_dir)
+# API Credentials
 
-# If your current working directory may change during script execution, it's recommended to
-# immediately convert program arguments to an absolute path. Then the variable root below will
-# be an absolute path as well. Example:
-# walk_dir = os.path.abspath(walk_dir)
-print('walk_dir (absolute) = ' + os.path.abspath(walk_dir))
+API_KEY = credentials.get_lastfm_api_key()
+API_SECRET = credentials.get_lastfm_api_secret()
+
+network = pylast.LastFMNetwork(api_key=API_KEY,
+                               api_secret=API_SECRET,
+                               )
+
+for artistName in os.listdir(artists_directory):
+    artist_directory = os.path.join(artists_directory, artistName)
+    for albumName in os.listdir(artist_directory):
+        album_directory = os.path.join(artist_directory, albumName)
+        # Assuming that all songs are of mp3 format
+        glob_parameter = os.path.join(album_directory,'*.mp3')
+        # songs_path will contain the absolute path to every mp3 file in album_directory
+        # reference: http://www.diveintopython.net/file_handling/os_module.html
+        songs_path = glob.glob(glob_parameter)
+        for audio_file_path in songs_path:
+            audio_file = eyed3.load(audio_file_path)
+            song_name = audio_file.tag.title
+            artist_name = audio_file.tag.artist
+            track_object = network.get_track(artist_name, song_name)
+            exit()
 
 
-for root, subdirs, files in os.walk(walk_dir):
-    if root==os.path.join(walk_dir,'English'):
-        print "artists %s"%files
