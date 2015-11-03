9864cd53d05cc8b59926a00eee3cb2851a5380a1
GauthamGoli <goligautham@gmail.com>
2015-11-03 18:29:18 +0530

    Increase size of fields

--- createdb.py
+++ createdb.py
@@ -1,5 +1,6 @@
 from schema import Base
 from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database, drop_database
 import credentials
 # import credentials
 db_name = credentials.get_db_name()
@@ -15,7 +16,12 @@ engine = create_engine('postgresql://{user}:{password}@{host}/{name}'.format(use
 
 
 if __name__ == '__main__':
+    # create db if it doesn't exist
+    if database_exists(engine.url):
+        print 'Dropping and recreating database'
+        drop_database(engine.url)
     try:
+        create_database(engine.url)
         Base.metadata.create_all(engine)
         print "database created."
     except Exception as e:
